605c357f0240ab7d0a588a712a6865cc477c9d66
Nicolas <nicolasandreose@gmail.com>
2026-03-31 20:11:58 -0300

    feat: add helper function to formatt price to brazilian currency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function formattedPrice(price: number) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+}

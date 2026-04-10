9ec63e11780597ba7a3a024472fb1c2615a7f6da
Nicolas <nicolasandreose@gmail.com>
2026-04-10 18:55:19 -0300

    feat: add wrapper privider to tests

---
+++ src/helpers/test-utils.tsx
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import type React from "react";
+import { UserContext } from "../contexts/user-context";
+import type { UserType } from "../../types/user-type";
+import { MemoryRouter } from "react-router";
+
+const renderWithProviders = (
+  component: React.ReactNode,
+  user: UserType | null = null,
+) => {
+  return render(
+    <MemoryRouter>
+      <UserContext.Provider
+        value={{
+          user,
+          loading: false,
+          loginUser: () => {},
+          signOutUser: () => {},
+          getUserByEmail: () => Promise.resolve(null),
+        }}
+      >
+        {component}
+      </UserContext.Provider>
+    </MemoryRouter>,
+  );
+};
+
+export default renderWithProviders;
